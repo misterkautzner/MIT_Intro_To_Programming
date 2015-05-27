5c139c0b11f1451f6a5f9a2bd9c7444cbc4eeb49
John Kautzner <misterkautzner@gmail.com>
2015-05-27 16:25:59 -0500

    Rounded to change and 'Result' statement

--- ps1_lecture_2.py
+++ ps1_lecture_2.py
@@ -1,23 +1,34 @@
 # Problem Set 1 (From Lecture 2)
 # Name: John Kautzner
 # Collaborators:  None
-# Time: 0:00
+# Time: 1:30
 #
 
 balance = float(raw_input("Enter the outstanding balance on your credit card: "))
 annualIntRate = float(raw_input("Enter the annual interest rate as a decimal: "))
 minMonPayRate = float(raw_input("Enter the minimum monthly payment rate as a decimal: "))
 intPaid = 0
+totalPaid = 0
 
 for month in range(1, 13):
     print "Month ", month
     
     minMonPay = minMonPayRate*balance
+    minMonPay = float('%.2f' % minMonPay)
     print "Minimum monthly payment: $", minMonPay
+
+    totalPaid += minMonPay
     
     intPaid = annualIntRate/12*balance
+    intPaid = float('%.2f' % intPaid)
+    
     princPaid = minMonPay - intPaid
     print "Principle paid: $", princPaid
 
     balance = balance - princPaid
     print "Remaining balance: $", balance
+
+print
+print "RESULT"
+print "Total amount paid: $", totalPaid
+print "Remaining balance: $", balance
