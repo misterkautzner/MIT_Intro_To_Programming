fb8e977d49be7b7e08c2ee3855f5e3ed490fa485
John Kautzner <misterkautzner@gmail.com>
2015-05-27 14:31:55 -0500

    basic program

--- ps1_lecture_2.py
+++ ps1_lecture_2.py
@@ -4,3 +4,20 @@
 # Time: 0:00
 #
 
+balance = float(raw_input("Enter the outstanding balance on your credit card: "))
+annualIntRate = float(raw_input("Enter the annual interest rate as a decimal: "))
+minMonPayRate = float(raw_input("Enter the minimum monthly payment rate as a decimal: "))
+intPaid = 0
+
+for month in range(1, 13):
+    print "Month ", month
+    
+    minMonPay = minMonPayRate*balance
+    print "Minimum monthly payment: $", minMonPay
+    
+    intPaid = annualIntRate/12*balance
+    princPaid = minMonPay - intPaid
+    print "Principle paid: $", princPaid
+
+    balance = balance - princPaid
+    print "Remaining balance: $", balance
